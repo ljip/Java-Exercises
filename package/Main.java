0291252b290f48f3c4989315723f6de869952d79
Filip Lilic <filiplilic@gmail.com>
2023-03-08 21:55:41 +0100

    first exercise

---
+++ package/Main.java
@@ -0,0 +1,8 @@
+import mypackage.MyPackage;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Now I'm in Main package");
+		MyPackage.sayHello();
+}
+}
---
+++ package/mypackage/MyPackage.java
@@ -0,0 +1,7 @@
+package mypackage;
+
+public class MyPackage {
+	public static void sayHello() {
+		System.out.println("Now I'm in MyPackage!");
+	}
+}
